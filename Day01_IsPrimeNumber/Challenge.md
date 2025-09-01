4e0fb5ff0027b25417875492940b4b2e1e250fbc
patilswapnapriya <patilswapnapriya@gmail.com>
2025-09-02 04:46:49 +0530

    updated Day01

---
+++ Day01_IsPrimeNumber/Challenge.md
@@ -0,0 +1,15 @@
+# Day 01 Challenge: Prime Number Check
+
+## Problem
+Write a Python function to check whether a given number is **prime**.
+
+A **prime number** is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+---
+
+## Input
+- An integer `n`.
+
+## Output
+- Return `True` if `n` is prime.
+- Return `False` otherwise.
--- Day01_IsPrimeNumber/Solution.py
+++ Day01_IsPrimeNumber/Solution.py
@@ -1,15 +1,23 @@
 
-
+# Day01 to fine if the Number is Prime or Not
 def is_prime(n):
+    """
+    Check if a number is prime.
+    Args:
+        n (int): Input number
+    Returns:
+        bool: True if n is prime, False otherwise
+    """
     if n<=1:
-        return False
+        return False # 0, 1, and negatives are not prime
+
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
-            return False
+            return False # if divisible by i or not.
     return True
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     test_numbers=[2,4,5,6,7,13,15,17,29]
     for num in test_numbers:
         print(f"{num} --> {is_prime(num)}")
